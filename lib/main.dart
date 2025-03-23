2689ffcdfd925464f0db0c9ab4b5c81f6c855646
NidhiPednekar <pednekarnidhi533@gmail.com>
2025-03-24 00:40:13 +0530

    Refactor to use environment variables for Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,11 @@ import 'homescreen.dart';
 import 'support.dart';
 import 'firebase_options.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
+  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
